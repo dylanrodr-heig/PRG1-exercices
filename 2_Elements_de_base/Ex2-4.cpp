a6f1ed025c309140f621082f8598f0688fed9f33
dylanrodr-heig <dylan.rodriguesalves@heig-vd.ch>
2020-09-18 12:03:16 +0200

    Ajout des exercice 2.1 -> 2.5

---
+++ 2_Elements_de_base/Ex2-4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main(){
+
+	cout << "signed char (" << numeric_limits<signed char>::digits + 1 << " bits) : "
+		  << (int)numeric_limits<signed char>::lowest() << " .. " << (int)numeric_limits<signed char>::max() << endl
+		  << "short (" << numeric_limits<signed short>::digits + 1 << " bits) : "
+		  << numeric_limits<signed short>::lowest() << " .. " << numeric_limits<signed short>::max() << endl
+		  << "int (" << numeric_limits<signed int>::digits + 1 << " bits) : "
+		  << numeric_limits<signed int>::lowest() << " .. " << numeric_limits<signed int>::max() << endl
+		  << "long (" << numeric_limits<signed long>::digits + 1 << " bits) : "
+		  << numeric_limits<signed long>::lowest() << " .. " << numeric_limits<signed long>::max() << endl
+		  << "long long (" << numeric_limits<signed long long>::digits + 1 << " bits) : "
+		  << numeric_limits<signed long long>::lowest() << " .. " << numeric_limits<signed long long>::max() << endl;
+
+	return EXIT_SUCCESS;
+}
+
